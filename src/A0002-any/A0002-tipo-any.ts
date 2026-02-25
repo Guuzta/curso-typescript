4d4f4c0c22c760ac2afa0d067f34937a10f60562
Guuzta <Guuhsilva13@outlook.com>
2026-02-25 14:54:43 -0300

    aula 02 tipo any

---
+++ src/A0002-any/A0002-tipo-any.ts
@@ -0,0 +1,8 @@
+//Utilize any apenas em último caso
+function showMessage(message: any) {
+  return message;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage("Hello world"));
+console.log(showMessage(22));
